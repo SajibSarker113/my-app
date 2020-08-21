68e0a7ee168db16667568c15a38a46d0b14af382
SajibSarker113 <66987905+SajibSarker113@users.noreply.github.com>
2020-08-21 21:25:38 +0600

    PASS DYNAMIC DATA TO COMPONENTS, PROPS IN REACT

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ function App() {
     <div className="App">
       <header className="App-header">
       <p>I am a react person</p>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Person name="Munna" job="Footballer"></Person>
+      <Person name="Sajib" job="Viewer"></Person>
+      <Person name="Jafor Iqbal" job="Nayok"></Person>
 
 
       </header>
     </div>
   );
 }
-function Person(){
+function Person(props){
   const personStyle = {
     border:'2px solid red', margin:'10px'
   }
   return (
-  <div style={{border:'2px solid yellow', margin:'10px'}}>
-    <h1>Name: Shakib Al Hasan</h1>
-    <h2>hero of the year</h2>
+  <div style={{border:'3px solid red', margin: '10px', width:'600px'}}>
+    <h1>Name: {props.name} </h1>
+    <h2>My profession: {props.job} </h2>
   </div>
   )
 }
