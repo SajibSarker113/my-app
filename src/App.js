4973f08a52207098eb5c72d510693c581e0604bf
SajibSarker113 <66987905+SajibSarker113@users.noreply.github.com>
2020-08-21 20:03:44 +0600

    first entry in js app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {name: 'Sajib Sarker',
+                job: 'web Developer'};
+var person2 = {name: 'Dipto Sarker',
+                job: 'software engineer'};
+var style = {color: 'red',
+              backgroundColor: 'yellow'
+           }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +17,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+  <h1 style={style} >My Heading: {person.name +" " + person.job} </h1>
+  <h1 style={{color: 'red', backgroundColor: 'cyan'}}>My Heading2: {person2.name +" " + person2.job} </h1>
+        <p>My first paragraph by emmet</p>
         <a
           className="App-link"
           href="https://reactjs.org"
